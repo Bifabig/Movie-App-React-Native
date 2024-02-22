b8cb27f652c7d0b3bae115b00aac2d0f967ddedf
Bifabig <biftubig@gmail.com>
2024-02-22 14:25:40 +0300

    app starter

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppNavigation from './app/Navigation/AppNavigation';
+
+const App = (): JSX.Element => {
+  return <AppNavigation />;
+};
+
+export default App;
---
+++ app/Screens/HomeScreen.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from 'react-native-heroicons/outline';
+import {styles} from '../theme';
+import TrendingMovies from '../components/TrendingMovies';
+
+const HomeScreen = () => {
+  const [trending, setTrending] = useState([1, 2, 3]);
+  return (
+    <View className="flex-1 bg-neutral-800">
+      <SafeAreaView className="mb-3">
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text className="text-white text-3xl font-bold">
+            <Text style={styles.text}>M</Text>ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={homeScreenStyles.paddingBottom}>
+        {/* Trending movies carousel */}
+        <TrendingMovies data={trending} />
+      </ScrollView>
+    </View>
+  );
+};
+
+const homeScreenStyles = {paddingBottom: 10};
+
+export default HomeScreen;
