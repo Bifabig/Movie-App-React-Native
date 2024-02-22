a8e43b4c389b6d739b38fe4af3096337cbb127e4
Bifabig <biftubig@gmail.com>
2024-02-22 17:30:24 +0300

    add movies to movie list component

--- app/components/MovieList.jsx
+++ app/components/MovieList.jsx
@@ -1,8 +1,21 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+  Dimensions,
+} from 'react-native';
 import React from 'react';
 import {styles} from '../theme';
+import {useNavigation} from '@react-navigation/native';
+
+let {width, height} = Dimensions.get('window');
 
 export default function MovieList({title, data}) {
+  const movieName = 'Avengers';
+  const navigation = useNavigation();
   return (
     <View className="mb-8 space-y-4">
       <View className="mx-4 flex-row justify-between items-center">
@@ -13,6 +26,31 @@ export default function MovieList({title, data}) {
           </Text>
         </TouchableOpacity>
       </View>
+      {/* Movie Row */}
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        // eslint-disable-next-line react-native/no-inline-styles
+        contentContainerStyle={{paddingHorizontal: 15}}>
+        {data.map((item, index) => {
+          return (
+            <TouchableWithoutFeedback
+              key={index}
+              onPress={() => navigation.navigate('Movie', item)}>
+              <View className="space-y-1 mr-4">
+                <Image
+                  source={require('../assets/images/poster_2.jpg')}
+                  className="rounded-3xl"
+                  style={{width: width * 0.33, height: height * 0.22}}
+                />
+                <Text className="text-neutral-200 ml-1">
+                  {movieName.length > 14 ? movieName.slice(0, 14) : movieName}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
