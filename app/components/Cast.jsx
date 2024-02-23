cd837ab5e684fec69fcba2f9771614dfe43ea4dd
Bifabig <biftubig@gmail.com>
2024-02-23 10:26:44 +0300

    add cast image

--- app/components/Cast.jsx
+++ app/components/Cast.jsx
@@ -1,7 +1,7 @@
-import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 
-export default function Cast({cast}) {
+export default function Cast({navigation, cast}) {
   const personName = 'Robert Downey Jr';
   const characterName = 'Tony Stark';
   return (
@@ -15,7 +15,16 @@ export default function Cast({cast}) {
         {cast &&
           cast.map((person, index) => {
             return (
-              <TouchableOpacity key={index} className="mr-4 items-center">
+              <TouchableOpacity
+                key={index}
+                className="mr-4 items-center"
+                onPress={() => navigation.navigate('Person', person)}>
+                <View className="overflow-hidden rounded-full w-20 h-20 border border-neutral-500 ">
+                  <Image
+                    className="rounded-2xl h-24 w-20"
+                    source={require('../assets/images/castImage_1.webp')}
+                  />
+                </View>
                 <Text className="text-white text-xs mt-1">
                   {characterName.length > 10
                     ? characterName.slice(0, 10) + '...'
